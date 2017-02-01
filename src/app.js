1993f2221fa56a29c9f502efba0b4229228e722b
Ryan Muncy <muncy.ryan@gmail.com>
2017-02-01 04:46:58 -0800

    Adds eslint and simplifies template

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 import 'index.html';
-import css from 'base.css';
-import _ from 'lodash/fp';
+import 'base.css';
 import Vue from 'vue';
 import App from 'app.vue';
 
-
 const ready = new Promise((resolve) => {
-  window.addEventListener("load", resolve, {once: false}, false);
+  window.addEventListener('load', resolve, {
+    once: false
+  }, false);
 });
 
-const run = function () {
-    const vm = new Vue({
-      el: '#app',
-      render: (h) => h(App)
-    });
+const run = function() {
+  new Vue({
+    el: '#app',
+    render: (h) => h(App)
+  });
 };
 
 ready.then(run);
